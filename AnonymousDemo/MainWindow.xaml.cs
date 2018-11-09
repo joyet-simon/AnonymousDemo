fb95cd521521498ef46962316ec6382dc8334935
joyet-simon <joyet.simon@hotmail.fr>
2018-11-09 10:00:34 +0100

    update 1

--- AnonymousDemo/MainWindow.xaml.cs
+++ AnonymousDemo/MainWindow.xaml.cs
@@ -27,21 +27,20 @@ namespace AnonymousDemo
             InitializeComponent();
             cbRegion.DisplayMemberPath = "nom";
             cbRegion.SelectedValuePath = "code";
-            cbRegion.Items.Add(new Region("FR-ARA", "Auvergne-Rhône-Alpes"));
-            cbRegion.Items.Add(new Region("FR-BFC", "Bourgogne-Franche-Comté"));
-            cbRegion.Items.Add(new Region("FR-BRE", "Bretagne"));
-            cbRegion.Items.Add(new Region("FR-CVL", "Centre-Val de Loire"));
-            cbRegion.Items.Add(new Region("FR-COR", "Corse"));
-            cbRegion.Items.Add(new Region("FR-GES", "Grand Est"));
-            cbRegion.Items.Add(new Region("FR- HDF", "Hauts-de-France"));
-            cbRegion.Items.Add(new Region("FR-IDF", "Île-de-France"));
-            cbRegion.Items.Add(new Region("FR-NOR", "Normandie"));
-            cbRegion.Items.Add(new Region("FR-NAQ", "Nouvelle-Aquitaine"));
-            cbRegion.Items.Add(new Region("FR-OCC", "Occitanie"));
-            cbRegion.Items.Add(new Region("FR-PDL", "Pays de la Loire"));
-            cbRegion.Items.Add(new Region("FR-PAC", "Provence-Alpes-Côte d'Azur"));
+            cbRegion.Items.Add(new { code = "FR-ARA", nom = "Auvergne-Rhône-Alpes" });
+            cbRegion.Items.Add(new { code = "FR-BFC", nom = "Bourgogne-Franche-Comté" });
+            cbRegion.Items.Add(new { code = "FR-BRE", nom = "Bretagne" });
+            cbRegion.Items.Add(new { code = "FR-CVL", nom = "Centre-Val de Loire" });
+            cbRegion.Items.Add(new { code = "FR-COR", nom = "Corse" });
+            cbRegion.Items.Add(new { code = "FR-GES", nom = "Grand-Est" });
+            cbRegion.Items.Add(new { code = "FR-IDF", nom = "Île-de-France" });
+            cbRegion.Items.Add(new { code = "FR-NOR", nom = "Normandie" });
+            cbRegion.Items.Add(new { code = "FR-NAQ", nom = "Nouvelle-Aquitaine" });
+            cbRegion.Items.Add(new { code = "FR-OCC", nom = "Occitanie" });
+            cbRegion.Items.Add(new { code = "FR-PDL", nom = "Pays-de-la-Loire" });
+            cbRegion.Items.Add(new { code = "FR-PAC", nom = "Provence-Alpes-Côte-d'Azur" });
         }
-       
+
         private void cbRegion_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var item = cbRegion.SelectedItem;
@@ -50,15 +49,3 @@ namespace AnonymousDemo
         }
     }
 }
-
-class Region
-{
-    public Region(String code, String nom)
-    {
-        this.code = code;
-        this.nom = nom;
-    }
-    public String code { get; set; }
-    public String nom { get; set; }
-}
-
